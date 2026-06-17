1ba888f38f2af40f202ca136a4171b04684f3c73
Dakota Nagy <dnagy6@student.cscc.edu>
2026-06-17 13:30:35 -0400

    Established math fucntions in circle.py and rectangle.py

---
+++ circle.py
@@ -0,0 +1,7 @@
+import math
+
+def calc_area(radius):
+    return math.pi * radius ** 2
+
+def calc_circumference(radius):
+    return 2 * math.pi * radius
---
+++ rectangle.py
@@ -0,0 +1,5 @@
+def calc_area(length, width):
+    return length * width
+
+def calc_perimeter(length, width):
+    return 2 * (length + width)
